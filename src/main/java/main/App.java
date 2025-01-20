24fec4c5a5a37ada893bc59714c1ddc1316c597d
Teme34 <tuomo.verho@gmail.com>
2025-01-20 20:47:30 +0200

    Versio 6

--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -12,7 +12,7 @@ public class App {
         Car newCar = new Car();
 
         Scanner sc = new Scanner(System.in);
-            System.out.print("  Anna auton merkki: ");
+            System.out.println("Anna auton merkki: ");
             String brand = sc.nextLine();
             newCar.setBrand(brand);
             System.out.print("Anna auton malli: ");
@@ -21,7 +21,11 @@ public class App {
 
         boolean exit = false;
         while(!exit) {
-            System.out.print("1) Näytä auton tila\n2) Muokkaa auton merkkiä ja mallia\n3) Kiihdytä autoa\n4) Hidasta autoa\n0) Lopeta ohjelma");
+            System.out.print("1) Näytä auton tila");
+            System.out.println("2) Muokkaa auton merkkiä ja mallia");
+            System.out.println("3) Kiihdytä autoa");
+            System.out.println("4) Hidasta autoa");
+            System.out.println("0) Lopeta ohjelma");
 // If statement is from the given example source code
             if(sc.hasNext()) {
                     int i = 0;
@@ -32,7 +36,7 @@ public class App {
                             newCar.status();
                             break;
                         case 2: 
-                            System.out.print("  Anna uusi auton merkki: ");
+                            System.out.print("Anna uusi auton merkki: ");
                             brand = sc.nextLine();
                             newCar.setBrand(brand);
                             System.out.print("Anna uusi auton malli: ");
@@ -40,7 +44,7 @@ public class App {
                             newCar.setModel(model);
                             break;
                         case 3:
-                            System.out.print("  Kuinka monta km/h haluat kiihdyttää? ");
+                            System.out.print("Kuinka monta km/h haluat kiihdyttää? ");
                             int acceleration = Integer.parseInt(sc.nextLine());
                             if (acceleration < 0) {
                                 System.out.print("Nopeuden täytyy olla positiivinen luku. ");
@@ -49,7 +53,7 @@ public class App {
                             newCar.accelerate(acceleration);
                             break;
                         case 4:
-                            System.out.print("  Kuinka monta km/h haluat hidastaa?? ");
+                            System.out.print("Kuinka monta km/h haluat hidastaa?? ");
                             int deceleration = Integer.parseInt(sc.nextLine());
                             if (deceleration < 0) {
                                 System.out.print("Nopeuden täytyy olla positiivinen luku. ");
