fb29e5f9e2adfec2533d1b522c9e8f126930fc6a
Teme34 <tuomo.verho@gmail.com>
2025-01-20 20:01:04 +0200

    first commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,77 @@
+// Name: Tuomo Verho
+// Student number: 001743638
+
+package main;
+
+import java.util.Scanner;
+
+
+public class App {
+    public static void main( String[] args ) {
+
+        Car newCar = new Car();
+
+        Scanner sc = new Scanner(System.in);
+            System.out.println("Anna auton merkki:");
+            String brand = sc.nextLine();
+            newCar.setBrand(brand);
+            System.out.println("Anna auton malli:");
+            String model = sc.nextLine();
+            newCar.setModel(model);
+
+        boolean exit = false;
+        while(!exit) {
+            System.out.println("1) Näytä auton tila\n2) Muokkaa auton merkkiä ja mallia\n3) Kiihdytä autoa\n4) Hidasta autoa\n0) Lopeta ohjelma");
+// If statement is from the given example source code
+            if(sc.hasNext()) {
+                    int i = 0;
+                    String stringinput = sc.nextLine();
+                    i = Integer.parseInt(stringinput);
+                    switch(i) {
+                        case 1:
+                            newCar.status();
+                            break;
+                        case 2: 
+                            System.out.println("Anna uusi auton merkki:");
+                            brand = sc.nextLine();
+                            newCar.setBrand(brand);
+                            System.out.println("Anna uusi auton malli:");
+                            model = sc.nextLine();
+                            newCar.setModel(model);
+                            break;
+                        case 3:
+                            System.out.println("Kuinka monta km/h haluat kiihdyttää?");
+                            int acceleration = Integer.parseInt(sc.nextLine());
+                            if (acceleration < 0) {
+                                System.out.println("Nopeuden täytyy olla positiivinen luku.");
+                                break;
+                            }
+                            newCar.accelerate(acceleration);
+                            break;
+                        case 4:
+                            System.out.println("Kuinka monta km/h haluat hidastaa??");
+                            int deceleration = Integer.parseInt(sc.nextLine());
+                            if (deceleration < 0) {
+                                System.out.println("Nopeuden täytyy olla positiivinen luku.");
+                                break;
+                            }
+                            newCar.decelerate(deceleration);
+                            break;
+                        case 0:
+                            System.out.println("Kiitos ohjelman käytöstä.");
+                            exit = true;
+                            break;
+                        default:
+                            System.out.println("Syöte oli väärä");
+                            break;
+
+
+                    }
+                }
+        }
+
+    }
+
+    
+
+}
---
+++ src/main/java/main/Car.java
@@ -0,0 +1,64 @@
+package main;
+
+public class Car {
+    private String brand;
+    private String model;
+    private int speed;
+
+    public Car() {
+        brand = "Skoda";
+        model = "Octavia";
+        speed = 0;
+
+    }
+
+    public Car(String brand, String model, int speed) {
+        this.brand = brand;
+        this.model = model;
+        this.speed = speed;
+    }
+
+    public void status() {
+        System.out.println(brand + " " + model + ", Nopeus: " + speed + " km/h");
+    
+    }
+
+    public void accelerate(int acceleration) {
+        speed = speed + acceleration;
+        this.speed = speed;
+    }
+
+    public void decelerate(int deceleration) {
+        speed = speed - deceleration;
+        this.speed = speed;
+    }
+
+     public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getModel() {
+        return model;
+    }
+    // 
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+       
+}
+
+
+
